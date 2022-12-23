cc3b0e9f494675678e155f2e1357eb23e88e83ff
Anna Prokhvataeva <Anna89851004453@yandex.ru>
2022-12-23 23:41:12 +0300

    Проверка 7"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,6 @@
+
+
 public class Main {
-    public static String hello() {
-        return "Hello my friend";
-    }
-
-    public static void main(String[] args) {
-        hello();
 
 
-    }
 }
