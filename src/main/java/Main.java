1889c047070f72cbde318fe372d8a87ef4e59f81
Anna Prokhvataeva <Anna89851004453@yandex.ru>
2022-12-23 23:26:20 +0300

    Проверка 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-//    public static String hello() {
-//        return "Hello";
-//    }
+    public static String hello() {
+        return "Hello";
+    }
 
     public static void main(String[] args) {
-//        hello();
+        hello();
 
 
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
-<html>
-<body>
-<h2>Hello from Spring MVC!!!</h2>
-</body>
-</html>
+<%--<html>--%>
+<%--<body>--%>
+<%--<h2>Hello from Spring MVC!!!</h2>--%>
+<%--</body>--%>
+<%--</html>--%>
